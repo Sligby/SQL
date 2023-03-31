cae994929cb43e83cf69125bc029e8141f0146f9
root <root@LAPTOP-N1AEFKV0.localdomain>
2023-03-30 21:22:11 -0400

    sql-join work

--- sql-joins/sql-joins/queries.sql
+++ sql-joins/sql-joins/queries.sql
@@ -1,1 +1,21 @@
 -- write your queries here
+SELECT * FROM owners 
+JOIN vehicles 
+ON (owners.id=vehicles.id)
+
+SELECT first_name, last_name, 
+COUNT(owner_id) FROM owners o 
+JOIN vehicles v on o.id=v.owner_id 
+GROUP BY (first_name, last_name) 
+ORDER BY first_name;
+
+SELECT first_name, last_name, 
+ROUND(AVG(price)) as average_price, 
+COUNT(owner_id) 
+FROM owners o 
+JOIN vehicles v on o.id=v.owner_id 
+GROUP BY 
+(first_name, last_name) 
+HAVING 
+COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 
+ORDER BY first_name DESC;
